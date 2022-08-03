9f5561d0a8c7dbe2fb7c6b4376d05aff7226ae47
Kraisser <vgaiko@mail.ru>
2022-08-03 17:31:54 +0300

    1.2 added Slider

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,9 @@ import '../css/variables.css';
 import '../css/header.css';
 import '../css/main.css';
 
+import $ from 'jquery';
+import slick from 'slick-carousel';
+
 import acordeon from './modules/accordeon';
 import videoPlayer from './modules/videoPlayer';
 
@@ -14,3 +17,12 @@ acordeon(accordeonList, '.svgAccordeon');
 const videoSrc = document.querySelector('#aboutArcPlayer');
 const playBut = document.querySelector('.aboutArcVideo .playerPlayBut');
 videoPlayer(videoSrc, playBut);
+
+$('#textSliderWrapper').slick({
+	dots: true,
+	infinite: true,
+	adaptiveHeight: true,
+	arrows: false,
+	cssEase: 'ease',
+	dotsClass: 'textSliderDots',
+});
